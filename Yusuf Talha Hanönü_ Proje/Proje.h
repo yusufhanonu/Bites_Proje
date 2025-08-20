f9dec577214517792000bd4f07029a1afd7ea0b6
Yusuf Talha Hanönü <hanonuyusuftalha1@gmail.com>
2025-08-20 03:36:32 +0300

    Gereksiz Kodlar Kaldırıldı
    Gereksiz ve kullanılmayan oluşturulmuş nesneler kaldırıldı ve kod kısaltıldı.

--- Yusuf Talha Hanönü_ Proje/Proje.h
+++ Yusuf Talha Hanönü_ Proje/Proje.h
@@ -7,22 +7,16 @@ extern int Boyut;
 
 class Proje{
 private:
-    int Deger;
-    int Engel;
+
     char Yazi;
 
 public:
-    Proje(int _Engel, int _Deger, char _Yazi);
-    void setEngel(int _Engel);
-    int getEngel();
-
-    void setDeger(int _Deger);
-    int getDeger() const;
-
+    Proje( char _Yazi);
+    
     void setYazi(char _Yazi);
     char getYazi() const;
 };
 
-void matrisBaslangic(vector<vector<Proje>> &vProje);
-void matrisYazdir(const vector<vector<Proje>>  &vProje);
-void matrisEngelAtama(vector<vector<Proje>> &vProje);
+void matrisBaslangic(vector<vector<Proje> > &vProje);
+void matrisYazdir(const vector<vector<Proje> >  &vProje);
+void matrisEngelAtama(vector<vector<Proje> > &vProje);
