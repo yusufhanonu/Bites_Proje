dcb7d3d3d549ef7594848eb9d2502de91ee73823
Yusuf Talha Hanönü <hanonuyusuftalha1@gmail.com>
2025-08-20 03:37:09 +0300

    Engel Fonksiyonu Düzeltildi
    Gereksiz ve kullanılmayan oluşturulmuş nesneler kaldırıldı ve kod kısaltıldı. Engellerin S ve X noktalarına tekrar engel koyması engellendi

--- Yusuf Talha Hanönü_ Proje/Proje.cpp
+++ Yusuf Talha Hanönü_ Proje/Proje.cpp
@@ -7,26 +7,10 @@
 using namespace std;
 int Boyut = 0;
 
-Proje::Proje(int _Engel, int _Deger, char _Yazi){
-    Engel = _Engel;
-    Deger = _Deger;
+Proje::Proje( char _Yazi){
     Yazi = _Yazi;
 }
 
-void Proje::setEngel(int _Engel){
-    Engel= _Engel;
-}
-
-int Proje::getEngel(){
-    return Engel;
-}
-void Proje::setDeger(int _Deger){
-    Deger= _Deger;
-}
-
-int Proje::getDeger()const {
-    return Deger;
-}
 void Proje::setYazi(char _Yazi){
     Yazi= _Yazi;
 }
@@ -49,17 +33,20 @@ void matrisYazdir(const vector<vector<Proje>> &vProje){
 void matrisBaslangic(vector<vector<Proje>> &vProje){
     int k,p;
     do {
-        cout << "Start Noktasını Sırasıyla 'Satır''Sütun' olarak giriniz: ";
-        cin>>k>>p;
+        cout << "Start noktasının 'Satırını' giriniz: ";
+        cin>>k;
+        cout << "Start noktasının 'Sütununu' giriniz: ";
+        cin>>p;
+
         if((k > (Boyut-1) or p > (Boyut-1)) or (k == (Boyut-1) and p == (Boyut-1))){
         cout<< "Bu degerler boyutu aşıyor veya Goal noktasına denk geliyor. "<<endl;
         }
     }while((k > (Boyut-1) or p > (Boyut-1)) or (k == (Boyut-1) and p == (Boyut-1)));
 
-    cout <<Boyut << "x" << Boyut << " Başlangıç matrisi yazdırılıyor.."<< endl;
+    cout <<Boyut << "x" << Boyut << " Başlangıç matrisiniz yazdırılıyor.."<< endl;
 
     vProje.clear();
-    vProje.resize(Boyut, vector<Proje>(Boyut, Proje (0,0,'0'))); //Sor
+    vProje.resize(Boyut, vector<Proje>(Boyut, Proje ('0'))); 
 
     vProje[k][p].setYazi('S');
     vProje[Boyut-1][Boyut-1].setYazi('G');
@@ -70,26 +57,25 @@ void matrisBaslangic(vector<vector<Proje>> &vProje){
 void matrisEngelAtama(vector<vector<Proje>> &vProje){
     int engel;
     srand(time(0));
-    int rastgeleSayi,rastgeleSayi1;
+    int x,j;
     
     do{
     cout << "Matrise atamak istediğiniz engel sayısınız seçiniz: ";
     cin >> engel;
-    if(engel>13 or engel == 0){
-        cout << "Program calıstırılamıyor, lütfen 1-13 arası bir deger giriniz "<<endl
+    if(engel>((Boyut*Boyut)-2) or engel == 0){
+        cout << "Program calıstırılamıyor, 1 ile " <<((Boyut*Boyut)-2) <<" arasında bir deger giriniz "<<endl
         <<endl;
     }
-    }while(engel > 13 or engel == 0);
+    }while(engel > ((Boyut*Boyut)-2) or engel == 0);
     
-    cout<< "Matrise " << engel << " Tane  Engel (X) Atanıyor..."<<endl;
+    cout<< "Matrise " << engel << " Tane  Engel (X) Atanıyor"<<endl;
         
         for(int i=0; i<engel; i++){
             do{ 
-                rastgeleSayi1=rand() % Boyut;
-                rastgeleSayi=rand() % Boyut;
-            }while((rastgeleSayi1 == 0 && rastgeleSayi == 0) || 
-            (rastgeleSayi1 == (Boyut-1) && rastgeleSayi == (Boyut-1)));     
-                vProje[rastgeleSayi1][rastgeleSayi].setYazi('X');     
+                x=rand() % Boyut;
+                j=rand() % Boyut;
+            }while((vProje[x][j].getYazi()=='S') or (vProje[x][j].getYazi()=='G') or (vProje[x][j].getYazi()=='X') );     
+                vProje[x][j].setYazi('X');     
     }
     matrisYazdir(vProje);
 }
